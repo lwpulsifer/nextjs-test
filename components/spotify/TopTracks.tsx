8a8c88458aef3c0a520847f35efc2945678f16e3
Liam Pulsifer <lpulsifer@ixl.com>
2021-11-30 18:12:41 -0500

    Fixed top tracks styling

--- components/spotify/TopTracks.tsx
+++ components/spotify/TopTracks.tsx
@@ -36,7 +36,7 @@ const TopTracks = () => {
     ?.[0];
 
   return (
-    <aside className={'flex flex-col p-2 rounded-3xl w-5/6 lg:w-2/3 xl:w-1/2 bg-highlight'}>
+    <aside className={'flex flex-col p-2 rounded-3xl bg-highlight w-full'}>
       <span className={'flex items-center justify-center text-2xl font-bold text-highlightHeader m-2'}>
         My Spotify Top Tracks
       </span>
