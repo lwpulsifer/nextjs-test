e169e1fd4e8f7683d3dff0cef7a31b3af2295223
Liam Pulsifer <lpulsifer@ixl.com>
2021-11-30 19:11:15 -0500

    Mostly working, but need to figure out how to style markdown stuff

---
+++ _posts/second-test.md
@@ -0,0 +1,17 @@
+---
+title: Older test blog
+slug: second-test
+desc: "A test post with a longer description. HLksjf;alkjsd aypogihlw htelkntnoug sdnflkdjj
+lksdfj fjwelkjrj fjowieugnn uruqntuifpsoioxoaldnen"
+date: 2021/11/28
+author: Liam Pulsifer
+---
+
+# This is my second blog post!
+
+## Second one :) 
+
+- Stuff's happening, baby!
+- Yep!
+- Keep it going
+- 
--- components/posts/MinimalPost.tsx
+++ components/posts/MinimalPost.tsx
@@ -1,13 +1,37 @@
 import { Post } from "../../lib/markdown/api";
 import Link from 'next/link';
+import useHoverState from "../../hooks/useHoverState";
 
 export default function MinimalPost({ post }: { post: Post }) {
+
+  const formattedDate = new Date(Date.parse(post.data.date)).toLocaleString("en-US", {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  });
+
+  const { isHovered, eventListeners } = useHoverState();
+
   return (
-    <li className="rounded-2xl border border-highlightHeader p-3 w-5/6">
-      <Link href={`/posts/${post.data.slug}`}>
-        {post.data.title}
+    <li 
+      className="rounded-2xl border border-highlightHeader p-3 w-5/6 hover:cursor-pointer hover:shadow-md my-2"
+      {...eventListeners}
+    >
+      <Link href={`/posts/${post.data.slug}`} passHref>
+        <section className="">
+          <div className={`flex justify-between ${isHovered ? 'underline' : 'no-underline'}`}>
+            <div className="flex flex-col">
+              <span>{post.data.title}</span>
+            </div>
+            <div>
+              <span>{formattedDate}</span>
+            </div>
+          </div>
+          <div className="no-underline">
+            <span>{post.data.desc}</span>
+          </div>
+        </section>
       </Link>
-      <div>{post.data.desc}</div>
     </li>
   )
 }
