41d4696c2acf1794647f11d4ea2f5de7905c4db0
Liam Pulsifer <lpulsifer@ixl.com>
2021-12-01 16:34:39 -0800

    Added headshot to index

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,19 +9,23 @@ const Home = () => {
     <BasePage>
       <div className={'flex flex-col items-center justify-center w-full mt-4'}>
         <BaseCard>
-          <div className="flex justify-between">
-            <h1 className={'text-4xl font-medium m-4 text-highlightHeader text-center'}>
-              Hi, I&apos;m Liam Pulsifer
-            </h1>
-            <Image src={'/grad_cropped.jpg'} width={36} height={36} alt={'Me'} />
-          </div>
-          <div className={'w-full md:w-11/12 rounded-xl text-center p-3 m-3'}>
-            I&apos;m a software engineer, writer, and amateur <a className={'text-link hover:underline'} href={'https://www.strava.com/athletes/47580246'}>athlete</a>. 
-            When I&apos;m not working at my day job at <a className={'text-link hover:underline'} href={'https://www.ixl.com/'}>IXL Learning</a>,
-            you can often find me reading, playing tennis, or surfing around the San Francisco bay area.
-            I&apos;m always looking for new connections, so don&apos;t be shy about getting in 
-            touch, and please feel free to peruse the various links on this site to get 
-            a sense of who I am and what I&apos;m doing. 
+          <div className="flex items-center justify-center">
+            <div className="flex flex-col w-3/4 items-center">
+              <h1 className={'text-4xl font-medium m-1 text-highlightHeader text-center'}>
+                Hi, I&apos;m Liam Pulsifer
+              </h1>
+              <div className={'w-full md:w-11/12 rounded-xl text-center p-3 m-1'}>
+                I&apos;m a software engineer, writer, and amateur <a className={'text-link hover:underline'} href={'https://www.strava.com/athletes/47580246'}>athlete</a>. 
+                When I&apos;m not working at my day job at <a className={'text-link hover:underline'} href={'https://www.ixl.com/'}>IXL Learning</a>,
+                you can often find me reading, playing tennis, or surfing around the San Francisco bay area.
+                I&apos;m always looking for new connections, so don&apos;t be shy about getting in 
+                touch, and please feel free to peruse the various links on this site to get 
+                a sense of who I am and what I&apos;m doing. 
+              </div>
+            </div>
+            <div className="relative rounded-full overflow-hidden flex justify-center items-center my-2">
+              <Image src={'/grad_cropped.jpg'} width={192} height={208} alt={'Me'} />
+            </div>
           </div>
         </BaseCard>
         <BaseCard>
