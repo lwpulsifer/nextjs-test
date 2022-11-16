84a58d2bfa1498bc038f4704db0334773ba5f0b4
Liam Pulsifer <lpulsifer@ixl.com>
2022-11-15 16:15:20 -0800

    Added api route for taking a screenshot of a page

---
+++ pages/api/take-screenshot.ts
@@ -0,0 +1,70 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import Cors from 'cors'
+import puppeteer from 'puppeteer';
+import { execFile } from 'child_process';
+import fs from 'fs';
+
+// Initializing the cors middleware
+// You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+const cors = Cors({
+  methods: ['GET', ],
+})
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(
+  req: NextApiRequest,
+  res: NextApiResponse,
+  fn: Function
+) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result: any) => {
+      if (result instanceof Error) {
+        return reject(result)
+      }
+
+      return resolve(result)
+    })
+  })
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  // Run the middleware
+  await runMiddleware(req, res, cors)
+
+  const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
+    const page = await browser.newPage();
+    await page.setViewport({ width: 600, height: 800 });
+    await page.goto(process.env.SCREENSHOT_URL || 'https://darksky.net/details/40.7127,-74.0059/2021-1-6/us12/en');
+    await page.screenshot({
+      path: '/tmp/screenshot.png',
+    });
+
+    await browser.close();
+
+    await convert('/tmp/screenshot.png');
+    const screenshot = fs.readFileSync('/tmp/screenshot.png');
+
+    res.writeHead(200, {
+      'Content-Type': 'image/png',
+      'Content-Length': screenshot.length,
+    });
+    return res.end(screenshot);
+}
+
+function convert(filename: string) {
+  return new Promise((resolve, reject) => {
+    const args = [filename, '-gravity', 'center', '-extent', '600x800', '-colorspace', 'gray', '-depth', '8', filename];
+    execFile('convert', args, (error, stdout, stderr) => {
+      if (error) {
+        console.error({ error, stdout, stderr });
+        reject();
+      } else {
+        resolve("Good");
+      }
+    });
+  });
+}
