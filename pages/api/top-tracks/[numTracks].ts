5d6330287e9a16901d28f7210cf7de4ecc9fc4a5
Liam Pulsifer <lpulsifer@ixl.com>
2022-11-21 11:39:44 -0800

    Another fix

--- pages/api/top-tracks/[numTracks].ts
+++ pages/api/top-tracks/[numTracks].ts
@@ -1,12 +1,16 @@
 import { getTopTracks } from "../../../lib/spotify";
 import { NextApiRequest, NextApiResponse } from "next";
 
+function stringify(s: string | string[]) {
+  return typeof s === 'string' ? s : s.join(",");
+}
+
 const topTracks = async (req: NextApiRequest, res: NextApiResponse) => {
   const response = await getTopTracks();
   const { items } = await response.json();
 
   const { numTracks } = req.query;
-  const tracks = items?.slice(0, Number.parseInt(numTracks, 10)).map((track) => ({
+  const tracks = items?.slice(0, Number.parseInt(stringify(numTracks), 10)).map((track) => ({
     artist: track.artists.map((_artist) => _artist.name).join(", "),
     album: track.album.name,
     songUrl: track.external_urls.spotify,
