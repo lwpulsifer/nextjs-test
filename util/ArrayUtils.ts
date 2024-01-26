2be566654f23300009b24627e4873d6e752d48ed
Liam Pulsifer <lpulsifer@ixl.com>
2024-01-26 11:54:09 -0800

    Progress

--- util/ArrayUtils.ts
+++ util/ArrayUtils.ts
@@ -3,3 +3,17 @@ export const sorted = <T>(arr: T[], cmp: (a: T, b: T) => number) => {
   newArr.sort(cmp);
   return newArr;
 };
+
+export const groupBy = <T>(arr: T[], groupingFunction: (groupItem: T) => string): {[key: string]: T[];} => {
+  const groups = {};
+  arr?.forEach((item) => {
+    const groupKey = groupingFunction(item);
+    if (groups[groupKey]) {
+     groups[groupKey].push(item);
+    } 
+    else {
+     groups[groupKey] = [item];
+    }
+  });
+  return groups;
+};
